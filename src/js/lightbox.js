52c666543af80809d01d5216a24495c0b0ec1666
klyukvushka <katyalex103@gmail.com>
2018-04-05 14:30:48 +0300

    browsersync and lightbox

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,19 +1,3 @@
-function cutText(text, number){
-  var cuted = text.slice(0,number);
-  return cuted + "...";
-}
-
-$(document).ready(function() {
-    var lettersToCut = 140;
-    var contentWrapper = $('.news-content__discription');
-    var contentText = contentWrapper.text();
-    contentWrapper.text(cutText(contentText, lettersToCut));
-    contentWrapper.append("<a href='#'>Read more</a>");
-  });
-
-
-
-
 /*!
  * Lightbox v2.10.0
  * by Lokesh Dhakar
